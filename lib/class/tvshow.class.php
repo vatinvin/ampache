3c69ae609a6c5e32df480fbfd63d1c607ff0608a
lachlan <lachlan.00@gmail.com>
2020-03-24 14:24:28 +1000

    bugs from phpstorm

--- lib/class/tvshow.class.php
+++ lib/class/tvshow.class.php
@@ -467,11 +467,11 @@ class TVShow extends database_object implements library_item
     {
         $deleted    = true;
         $season_ids = $this->get_seasons();
-        foreach ($season_ids as $season) {
-            $season  = new TVShow_Season($season);
+        foreach ($season_ids as $season_object) {
+            $season  = new TVShow_Season($season_object);
             $deleted = $season->remove_from_disk();
             if (!$deleted) {
-                debug_event('tvshow.class', 'Error when deleting the season `' . $season . '`.', 1);
+                debug_event('tvshow.class', 'Error when deleting the season `' . (string) $season . '`.', 1);
                 break;
             }
         }
